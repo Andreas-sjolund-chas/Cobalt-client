9a8e845b9aeec8de04d4d585d4996b59dce7abc9
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-05-10 16:25:34 +0200

    add presentations to workspace modal

--- src/Views/Dashboard/Workspaces/Presentations.js
+++ src/Views/Dashboard/Workspaces/Presentations.js
@@ -2,13 +2,72 @@ import React from "react";
 import { css, withStyles } from "../../../withStyles";
 import FlexContainer from "../../../Containers/FlexContainer";
 import Heading from "../../../Elements/Heading";
+import Paragraph from "../../../Elements/Paragraph";
+import List from "../../../Elements/List";
+import ListItem from "../../../Elements/ListItem";
+import ButtonLink from "../../../Elements/ButtonLink";
 
 const Presentations = ({ styles, ...props }) => {
-  return <Heading size="3">Presentations</Heading>;
+  console.log(props.data);
+  return (
+    <FlexContainer fullWidth="1">
+      <div {...css(styles.presentationsInfo)}>
+        <Paragraph size="sub">
+          Total presentations held in this workspace:{" "}
+          {props.data.presentations.length}
+        </Paragraph>
+      </div>
+      <div {...css(styles.presentations)}>
+        <Paragraph style={{ margin: "0" }} size="sub">
+          Your latest presentations in this workspace:
+        </Paragraph>
+        <List style={{ width: "100%" }}>
+          {props.data.presentations.map((p, i) => {
+            if (i > 4) return;
+            return (
+              <div {...css(styles.listItems)}>
+                <ListItem
+                  key={i}
+                  iconPosition="right"
+                  iconFillColor="darkMetal"
+                >
+                  <Paragraph size="sub">{p.date}</Paragraph>
+                  <Paragraph size="sub">Name: {p.name}</Paragraph>
+                  <Paragraph size="sub">Attendees: {p.attendees}</Paragraph>
+                </ListItem>
+              </div>
+            );
+          })}
+        </List>
+      </div>
+    </FlexContainer>
+  );
 };
 
 export default withStyles(({ themes, colors }) => {
   return {
-    presentations: {}
+    presentationsInfo: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "flex-start",
+      width: "100%",
+      borderBottom: "1px solid black",
+      padding: "20px"
+    },
+    presentations: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "flex-start",
+      width: "100%",
+      padding: "20px"
+    },
+    listItems: {
+      width: "100%",
+      borderBottom: "1px solid black",
+      paddingBottom: "10px",
+      ":nth-child(1n)": {
+        marginTop: "20px"
+      }
+    }
   };
 })(Presentations);
