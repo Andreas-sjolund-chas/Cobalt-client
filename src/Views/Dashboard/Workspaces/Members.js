2abb494debcb861a1a24e82f9d8035afd1dadd2c
Andreas Sjölund <andreas.sjolund@chasacademy.se>
2018-05-10 11:12:34 +0200

    fixed errors

--- src/Views/Dashboard/Workspaces/Members.js
+++ src/Views/Dashboard/Workspaces/Members.js
@@ -28,7 +28,7 @@ const Members = ({
             <ListItem {...css(styles.listItem)}>
               <FlexContainer direction="row" align="center" justify="between">
                 <Paragraph size="sub" style={{ marginBottom: "0px" }}>
-                  {member.name}
+                  {member.name} - {member.email}
                 </Paragraph>
                 -
                 <Paragraph size="sub" style={{ marginBottom: "0px" }}>
