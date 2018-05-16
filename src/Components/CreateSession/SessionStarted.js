2c7af81ed77bcdd3a970f45df9b8737a190872d3
Andreas Sjölund <andreas.sjolund@chasacademy.se>
2018-05-16 16:59:24 +0200

    add logo to QR-popup

--- src/Components/CreateSession/SessionStarted.js
+++ src/Components/CreateSession/SessionStarted.js
@@ -6,6 +6,8 @@ import { Link } from "react-router-dom";
 import FlexContainer from "../../Containers/FlexContainer";
 import Heading from "../../Elements/Heading";
 import Paragraph from "../../Elements/Paragraph";
+import Button from "../../Elements/Button";
+import ButtonLink from "../../Elements/ButtonLink";
 import CopyTextfield from "../../Elements/CopyTextfield";
 import QrCodeWindow from "../QrCodeWindow";
 
@@ -23,35 +25,36 @@ const SessionStarted = ({ sessionId = null }) => (
       url={`${process.env.REACT_APP_CLIENT_BASE_URL}/session/${sessionId}`}
       style={{ width: "100%" }}
     />
-    <Link
-      to={`${process.env.REACT_APP_CLIENT_BASE_URL}/qr/${sessionId}`}
-      target="_blank"
-      onClick={event => {
-        event.preventDefault();
-        window.open(
-          `${process.env.REACT_APP_CLIENT_BASE_URL}/qr/${sessionId}`,
-          "Popup",
-          "location,status,scrollbars,resizable,width=200,height=200"
-        );
-      }}
-    >
-      <FlexContainer direction="row">
-        <QRCode
-          value={sessionId}
-          renderAs="svg"
-          size="70"
-          style={{ padding: "1px", backgroundColor: "#FFF" }}
-          level="H"
-        />
-        <Paragraph
-          style={{ marginLeft: "5px" }}
-          size="normal"
-          appearance="white"
-        >
-          Click to open QR-Code in separate window
-        </Paragraph>
-      </FlexContainer>
-    </Link>
+    <FlexContainer direction="row">
+      <QRCode
+        value={sessionId}
+        renderAs="svg"
+        size="70"
+        style={{ padding: "1px", backgroundColor: "#FFF" }}
+        level="H"
+      />
+    </FlexContainer>
+    <FlexContainer direction="row">
+      <Button style={{ marginLeft: "5px" }} appearance="secondary">
+        Copy QR-code
+      </Button>
+      <ButtonLink
+        appearance="secondary"
+        to={`${process.env.REACT_APP_CLIENT_BASE_URL}/qr/${sessionId}`}
+        target="_blank"
+        onClick={event => {
+          const size = window.innerHeight * 0.75;
+          event.preventDefault();
+          window.open(
+            `${process.env.REACT_APP_CLIENT_BASE_URL}/qr/${sessionId}`,
+            "Popup",
+            `location,status,resizable,centerscreen,width=${size},height=${size}`
+          );
+        }}
+      >
+        Open in new window
+      </ButtonLink>
+    </FlexContainer>
   </FlexContainer>
 );
 
--- src/Components/QrCodeWindow.js
+++ src/Components/QrCodeWindow.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
+
+import { css, withStyles } from "react-with-styles";
+
 import QRCode from "qrcode.react";
 import ReactDOM from "react-dom";
+
+import Card from "../Elements/Card";
+import Heading from "../Elements/Heading";
 
 const url = window.location.href;
 
@@ -11,16 +17,38 @@ class QrCodeWindow extends Component {
     this.sessionId = this.href.substr(this.href.lastIndexOf("/") + 1);
   }
   render() {
+    const { styles } = this.props;
+
     return (
-      <QRCode
-        value={this.sessionId}
-        renderAs="svg"
-        size="100vh"
-        style={{ padding: "20px", backgroundColor: "#FFF" }}
-        level="H"
-      />
+      <Card style={{ height: "calc(100% - 48px)", width: "100%" }}>
+        <Heading {...css(styles.logo)} size="2">
+          Feed<span>.io</span>
+        </Heading>
+        <QRCode
+          value={this.sessionId}
+          renderAs="svg"
+          size="100%"
+          style={{
+            width: "100%",
+            height: "100%",
+            padding: "20px",
+            backgroundColor: "#FFF"
+          }}
+          level="H"
+        />
+      </Card>
     );
   }
 }
 
-export default QrCodeWindow;
+export default withStyles(({ colors }) => {
+  return {
+    logo: {
+      color: "#FFF",
+      marginBottom: "0px",
+      ":nth-child(1n) span": {
+        color: colors.danger
+      }
+    }
+  };
+})(QrCodeWindow);
